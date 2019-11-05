cde3eb9a09ad314b675c6032b824c52a650bccba
natashabutorova <48949449+natashabutorova@users.noreply.github.com>
2019-11-05 21:18:32 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
-#include <iostream> 
-int main() 
-{ 
-static_assert("C++17"); 
-std::cout << “Hello world!\n”; 
+#include <iostream>
+int main()
+{
+static_assert("C++17");
+std::cout << "Hello world\n";
 }
